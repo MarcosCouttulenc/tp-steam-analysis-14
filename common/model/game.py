b8d0fc3b97ca67d607c2108937a1b6a9cdb2769a
Tomas Castellano <tcastellano@fi.uba.ar>
2024-10-03 03:06:07 -0300

    feat: agrego modelos

---
+++ common/model/game.py
@@ -0,0 +1,4 @@
+class Game:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
---
+++ common/model/review.py
@@ -0,0 +1,4 @@
+class Review:
+    def __init__(self, game_id, review):
+        self.game_id = game_id
+        self.review = review
