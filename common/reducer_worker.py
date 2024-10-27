7913f9e18dd7d72b7d9e6dd658f6b6539d8d93e8
MarcosC <mcouttulenc@fi.uba.ar>
2024-10-27 17:37:36 -0300

    refactor de reducer 2

---
+++ common/reducer_worker.py
@@ -0,0 +1,66 @@
+import logging
+logging.basicConfig(level=logging.CRITICAL)
+from middleware.queue import ServiceQueues
+from common.message import Message
+from common.message import MessageGameInfo
+from common.message import MessageQueryTwoFileUpdate
+from common.message import MessageEndOfDataset
+
+CHANNEL_NAME =  "rabbitmq"
+
+class ReducerWorker:
+    def __init__ (self,queue_name_origin, queues_name_destiny_str):
+        self.queue_name_origin = queue_name_origin
+        self.queues_name_destiny = queues_name_destiny_str.split(",")
+        self.running = True
+        self.service_queues = ServiceQueues(CHANNEL_NAME)
+        
+        self.buffer = self.init_buffer()
+
+
+    def start(self):
+        while self.running:
+            self.service_queues.pop(self.queue_name_origin, self.process_message)
+
+    def update_buffer(self, message: Message):
+        return 0
+
+    def send_buffer_to_file(self):
+        return 0
+    
+    def init_buffer(self):
+        return 0
+    
+    def buffer_contains_items(self):
+        return True
+
+    def buffer_is_full(self):
+        return True
+
+    def process_message(self, ch, method, properties, message: Message):
+        if message.is_eof():
+            self.handle_eof(ch, method, properties, message)
+            return
+    
+        self.update_buffer(message)
+
+        if self.buffer_is_full():
+            self.send_buffer_to_file()
+
+        self.service_queues.ack(ch, method)
+
+    def handle_eof(self, ch, method, properties, message: Message):
+        msg_eof = MessageEndOfDataset.from_message(message)
+        
+        if  msg_eof.is_last_eof():
+            print("push eof")
+        
+        if self.buffer_contains_items():
+            self.send_buffer_to_file()
+
+        self.running = False
+        self.service_queues.ack(ch, method)
+        self.service_queues.close_connection()
+    
+    
+    
--- query2_reducer/query2_reducer.py
+++ query2_reducer/query2_reducer.py
@@ -1,58 +1,31 @@
 import logging
 logging.basicConfig(level=logging.CRITICAL)
-from middleware.queue import ServiceQueues
-from common.message import Message
+
 from common.message import MessageGameInfo
 from common.message import MessageQueryTwoFileUpdate
-from common.message import MessageEndOfDataset
+from common.reducer_worker import ReducerWorker
 
 CHANNEL_NAME =  "rabbitmq"
 BUFFER_MAX_SIZE = 0
 
-class QueryTwoReducer:
-    def __init__(self, queue_name_origin, queues_name_destiny_str):
-        self.queue_name_origin = queue_name_origin
-        self.queues_name_destiny = queues_name_destiny_str.split(",")
-        self.running = True
-        self.service_queues = ServiceQueues(CHANNEL_NAME)
-        self.buffer = [] # Se van a guardar tupla de (nombre, playtime)
-    
-    def start(self):
-        while self.running:
-            self.service_queues.pop(self.queue_name_origin, self.process_message)
-
-    
-    def process_message(self, ch, method, properties, message: Message):
-        if message.is_eof():
-            msg_eof = MessageEndOfDataset.from_message(message)
-            if  msg_eof.is_last_eof():
-                print("push eof")
-            
-            if len(self.buffer) > 0:
-                self.save_buffer_in_file_and_clean_it()
-
-            self.running = False
-            self.service_queues.ack(ch, method)
-            self.service_queues.close_connection()
-            return
-    
+class QueryTwoReducer(ReducerWorker):
+    def update_buffer(self, message):
         msg_game_info = MessageGameInfo.from_message(message)
         self.buffer.append((msg_game_info.game.name, msg_game_info.game.playTime))
         self.buffer.sort(key=lambda game_data: game_data[1], reverse=True)
 
-        if len(self.buffer) >= BUFFER_MAX_SIZE:
-            self.save_buffer_in_file_and_clean_it()
-
-        self.service_queues.ack(ch, method)
-
-    def save_buffer_in_file_and_clean_it(self):
-        # toma lo que hay en el buffer, lo guarda en el archivo y lo mantiene ordenado de mayor a menor segun
-        # tiempo de juego.
+    def send_buffer_to_file(self):
         for queue_name in self.queues_name_destiny:
             msg = MessageQueryTwoFileUpdate(self.buffer)
-
-            #print(f"Save buffer Q2 - {msg}")
-
             self.service_queues.push(queue_name, msg)
         
         self.buffer = []
+    
+    def init_buffer(self):
+        return []
+    
+    def buffer_contains_items(self):
+        return len(self.buffer) > 0
+
+    def buffer_is_full(self):
+        return len(self.buffer) >= BUFFER_MAX_SIZE
