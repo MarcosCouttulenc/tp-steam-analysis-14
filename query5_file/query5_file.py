c88c40893a89ff6ca7d57a40c804da7131ed334d
MarcosC <mcouttulenc@fi.uba.ar>
2024-10-28 18:06:00 -0300

    query5_file refactorizado

--- query5_file/query5_file.py
+++ query5_file/query5_file.py
@@ -9,69 +9,14 @@ from common.protocol import Protocol
 from common.protocol import *
 import multiprocessing
 import socket
-import errno
+import errno       
+from common.query_file_worker import QueryFile
 
-CHANNEL_NAME = "rabbitmq"
+class QueryFiveFile(QueryFile):
+    def get_message_result_from_file_snapshot(self, client_id, file_snapshot):
+        message_result = MessageQueryFiveResult(client_id, file_snapshot)
+        return message_result
 
-class QueryFiveInfo:
-    def __init__(self, id, name, cant_neg):
-        self.id = id
-        self.name = name
-        self.cant_neg = cant_neg
-    
-    def sum_cant_neg(self, new_cant_neg):
-        self.cant_neg += new_cant_neg
-    
-    
-class QueryFiveFile:
-    def __init__(self, queue_name_origin, file_path, result_query_port, listen_backlog):
-        self.queue_name_origin = queue_name_origin
-        self.file_path = file_path
-        self.new_connection_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.new_connection_socket.bind(('', result_query_port))
-        self.new_connection_socket.listen(listen_backlog)
-        self.file_lock = multiprocessing.Lock()
-        self.running = True
-        self.service_queues = ServiceQueues(CHANNEL_NAME)
-        manager = multiprocessing.Manager()
-        self.totals = manager.dict()
-    
-    def start(self):
-        process_updates = multiprocessing.Process(target=self.process_handle_result_updates, args=())
-        process_queries = multiprocessing.Process(target=self.process_handle_result_queries, args=())
-
-        process_updates.start()
-        process_queries.start()
-
-        process_updates.join()
-        process_queries.join()
-    
-    def process_handle_result_queries(self):
-        while self.running:
-            client_sock = self.__accept_new_connection()
-            protocol = Protocol(client_sock)
-
-            message = protocol.receive()
-            client_id = message.get_client_id()
-            
-            with self.file_lock:
-                file_snapshot = self.get_file_snapshot(client_id)
-                
-            message_result = MessageQueryFiveResult(client_id, file_snapshot)
-            protocol.send(message_result)
-
-    def __accept_new_connection(self):
-        try:
-            c, addr = self.new_connection_socket.accept()
-            logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
-            return c
-        except OSError as e:
-            if e.errno == errno.EBADF:  # Bad file descriptor, server socket closed
-                logging.info('SOCKET CERRADO - ACCEPT_NEW_CONNECTIONS')
-                return None
-            else:
-                raise
-            
     def get_file_snapshot(self, client_id):
         file_snapshot = []
 
@@ -86,6 +31,18 @@ class QueryFiveFile:
         
         return file_snapshot[:10]
 
+    def update_results(self, message):
+        msg_query_five_file_update = MessageQueryFiveFileUpdate.from_message(message)
+        for (name, cant_pos, cant_neg,game_id) in msg_query_five_file_update.buffer:
+            if name not in self.totals:
+                self.totals[name] = [0, 0, int(game_id)]
+                
+            temp = self.totals[name]
+            temp[0] += int(cant_pos)
+            temp[1] += int(cant_neg)
+            self.totals[name] = temp
+    
+
     def get_percentil_90(self):
         if len(self.totals) == 0:
             return None
@@ -96,33 +53,5 @@ class QueryFiveFile:
 
         percentil_90 = neg_reviews_sorted[percentil_90_pos]
         return percentil_90
-
-    def process_handle_result_updates(self):
-        while self.running:
-            self.service_queues.pop(self.queue_name_origin, self.handle_new_update)
-
-    def handle_new_update(self, ch, method, properties, message: Message):
-        msg_query_five_file_update = MessageQueryFiveFileUpdate.from_message(message)
-        
-        with self.file_lock:
-            self.update_totals(msg_query_five_file_update)
-
-        self.service_queues.ack(ch, method)
-
-
-    def update_totals(self, msg_query_five_file_update):
-        for (name, cant_pos, cant_neg,game_id) in msg_query_five_file_update.buffer:
-            if name not in self.totals:
-                self.totals[name] = [0, 0, int(game_id)]
-                
-            temp = self.totals[name]
-            temp[0] += int(cant_pos)
-            temp[1] += int(cant_neg)
-            self.totals[name] = temp
-
-
-        
-        
-                    
 
 
