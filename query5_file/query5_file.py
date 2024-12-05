71c124b2fc9509943249d34d9aa15706e3f535c8
ferburs <fbursztyn@fi.uba.ar>
2024-12-05 06:20:23 -0300

    filtro query5_file
    Co-authored-by: MarcosCouttulenc <MarcosCouttulenc@users.noreply.github.com>
    Co-authored-by: tcastellano00 <tcastellano00@users.noreply.github.com>

--- query5_file/query5_file.py
+++ query5_file/query5_file.py
@@ -1,5 +1,6 @@
 import csv
 import logging
+import os
 logging.basicConfig(level=logging.CRITICAL)
 
 from common.message import MessageReviewInfo
@@ -15,24 +16,22 @@ class QueryFiveFile(QueryFile):
 
     def get_file_snapshot(self, client_id):
         file_snapshot = []
-        client_file_path = self.get_file_path_client(client_id)
         
-        data = self.get_data_from_file(client_file_path)
+        data = self.get_file_info(client_id)
 
         percentil_90 = self.get_percentil_90(data)
 
-        for name, cant_reviews in sorted(data.items(), key=lambda x: x[1][2], reverse=False):
-            if cant_reviews[1] > percentil_90:
-                file_snapshot.append((cant_reviews[2], name))
+        for name, game_info in sorted(data.items(), key=lambda x: x[1][2], reverse=False):
+            if game_info[1] > percentil_90:
+                file_snapshot.append((game_info[2], name))
 
         return file_snapshot[:10]
     
     def update_results(self, message):
         msg_review_info = MessageReviewInfo.from_message(message)
         client_id = str(msg_review_info.get_client_id())
-        client_file_path = self.get_file_path_client(client_id)
 
-        data = self.get_data_from_file(client_file_path)
+        data = self.get_file_info(client_id)
 
         if not msg_review_info.review.game_name in data:
             data[msg_review_info.review.game_name] = [0, 0, msg_review_info.review.game_id]
@@ -42,12 +41,7 @@ class QueryFiveFile(QueryFile):
         else:
             data[msg_review_info.review.game_name][1] += 1
 
-        with open(client_file_path, mode='w') as file:
-            fieldnames = ['game', 'cant_pos', 'cant_neg', 'game_id']
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            for game, cant_reviews in data.items():
-                writer.writerow({'game': game, 'cant_pos': cant_reviews[0], 'cant_neg': cant_reviews[1], 'game_id': cant_reviews[2]})
+        self.update_results_in_disk(client_id, data)
     
 
     def get_percentil_90(self, data):
@@ -61,20 +55,102 @@ class QueryFiveFile(QueryFile):
         percentil_90 = neg_reviews_sorted[percentil_90_pos]
         return percentil_90
 
-    def get_data_from_file(self, client_file_path):
-        data = {}
+        
+    def get_file_info(self, client_id):
+        client_file_path = self.get_file_path_client(client_id)
+
+        games = {}
 
         try :
             with open(client_file_path, mode='r') as file:
                 reader = csv.DictReader(file)
                 for row in reader:
-                    data[row['game']] = [int(row['cant_pos']), int(row['cant_neg']), int(row['game_id'])]
+                    games[row['game']] = [int(row['cant_pos']), int(row['cant_neg']), int(row['game_id'])]
         except FileNotFoundError:
             pass
 
-        return data
+        return games
+
+    def update_results_in_disk(self, client_id, games_info):
+        client_file_path = self.get_file_path_client(client_id)
+
+        with open(client_file_path, mode='w') as file:
+            fieldnames = ['game', 'cant_pos', 'cant_neg', 'game_id']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            for game, info in games_info.items():
+                writer.writerow({'game': game, 'cant_pos': info[0], 'cant_neg': info[1], 'game_id': info[2]})
     
 
     def get_file_path_client(self, client_id):
         return f"{client_id}_{self.file_path}"
 
+
+
+    ## Transaction Log
+    ## --------------------------------------------------------------------------------
+    def get_transaction_log(self, message):
+        msg_review_info = MessageReviewInfo.from_message(message)
+        client_id = str(msg_review_info.get_client_id())
+        file_info = self.get_file_info(client_id)
+        game_name = msg_review_info.review.game_name
+
+        if game_name in file_info.keys():
+            values = file_info[game_name] 
+        else:
+            values = [0,0,0]
+        
+        new_values = values
+
+        if msg_review_info.review.is_positive():
+            new_values[0] += 1
+        else:
+            new_values[1] += 1
+    
+        transaction_log = f"msg::{message.get_message_id()}|client::{client_id}|game::{game_name}|game_id::{msg_review_info.review.game_id}|prev::{values[0]}%!{values[1]}|actual::{new_values[0]}%!{new_values[1]}"
+        return transaction_log 
+
+        
+    def recover_from_transaction_log(self):
+        if not os.path.exists(self.path_logging):
+            os.makedirs(os.path.dirname(self.path_logging), exist_ok=True)
+            return
+        
+        print("Empieza recuperacion del log \n")
+        with open(self.path_logging, 'r') as file:
+            line = file.readline().strip()
+
+            print(f"Nos levantamos y el log tiene: {line}")
+
+            data = line.split("|")
+
+            msg_id = data[0].split("::")[1]
+            client_id = data[1].split("::")[1]
+            game_name = data[2].split("::")[1]
+            game_id = data[3].split("::")[1]
+            previous_state = data[4].split("::")[1]
+            actual_state = data[5].split("::")[1]
+
+        file_info = self.get_file_info(client_id)
+
+        to_print = "No estaba en el dict"
+        if game_name in file_info.keys():
+            to_print = file_info[game_name]
+        print(f"Antes de recuperar: {to_print}")
+        
+        aux = []
+        if not game_name in file_info.keys():
+            aux = [0, 0, game_id]
+        else:
+            aux = file_info[game_name]
+
+        pos_neg = actual_state.split("%!")
+
+        aux[0] = pos_neg[0]
+        aux[1] = pos_neg[1]
+
+        file_info[game_name] = aux
+
+        self.last_msg_id_log_transaction = msg_id
+        self.update_results_in_disk(client_id, file_info)
+        
